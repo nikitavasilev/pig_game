60be43ba7dbf8cda35b0d9429a0007eda6443a82
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-02-03 19:25:21 +0100

    same in a row ft

--- main.js
+++ main.js
@@ -11,23 +11,50 @@ GAME RULES:
 var scores, roundScore, activePlayer, gamePlaying;
 init();
 
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
+    var previousDice = [];
 
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'img/dice-' + dice + '.png';
+    previousDice.push(dice);
     
     // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
+    if (dice !== 1 && previousDice[-1] === dice) {
+      roundScore === 0;
+      nextPlayer();
+    } else if (dice !== 1 && previousDice[-1] !== dice) {
       // Add score
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       // Next player
+      previousDice = [];
       nextPlayer();
     }
   }
@@ -72,25 +99,22 @@ function nextPlayer() {
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
-function init() {
-  scores = [0, 0];
-  activePlayer = 0;
-  roundScore = 0;
-  gamePlaying = true;
-
-  document.querySelector('.dice').style.display = 'none';
-
-  document.getElementById('score-0').textContent = '0';
-  document.getElementById('score-1').textContent = '0';
-  document.getElementById('current-0').textContent = '0';
-  document.getElementById('current-1').textContent = '0';
-  document.getElementById('name-0').textContent = 'Player 1';
-  document.getElementById('name-1').textContent = 'Player 2';
-  document.querySelector('.player-0-panel').classList.remove('winner');
-  document.querySelector('.player-1-panel').classList.remove('winner');
-  document.querySelector('.player-0-panel').classList.remove('active');
-  document.querySelector('.player-1-panel').classList.remove('active');
-  document.querySelector('.player-0-panel').classList.add('active');
-}
-
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row.
+After that, it's the next player's turn.
+(Hint: Always save the previous dice roll in a separate variable)
+
+2. Add an input field to the HTML where players can set the winning score,
+so that they can change the predefined score of 100.
+(Hint: you can read that value with the .value property in JavaScript.
+  This is a good oportunity to use google to figure this out :)
+
+3. Add another dice to the game, so that there are two dices now.
+The player looses his current score when one of them is a 1.
+(Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
